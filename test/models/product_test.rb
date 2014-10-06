1df78d935641bb32ab69f343f285467fb1239d5d
majunhua <326384004@qq.com>
2014-10-06 15:05:10 +0800

    second

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "attribute must not be empty" do
+  product = Product.new#"{#(:title => "My Book Title" ,
+                         #:description => "yyy" ,
+                         #:price => 1,
+                        #:image_url => "123.png")}"
+
+  assert product.invalid?
+  assert product.errors[:title].any?
+  assert product.errors[:description].any?
+  assert product.errors[:image_url].any?
+  assert product.errors[:price].any?
+   end
 end
